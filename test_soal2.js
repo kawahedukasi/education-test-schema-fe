024cc9798c634e2a9e485d1ae12ea7960ce6bfdf
ADRIAN MIFTAHUL HAQ <104409055+AdrianMiftahulH@users.noreply.github.com>
2022-07-13 01:30:15 +0700

    Adrian Miftahul Haq  : Test Kawah Edukasi bath III

--- test_soal2.js
+++ test_soal2.js
@@ -1,16 +1,37 @@
-function validation1() {
-   var validasiHuruf = /^[a-zA-Z ]+$/;
-   var input1 = "hallo jesika24 selamat datang!";
 
+var input1 = "hallo jesika24 selamat datang!";
+var input2 = "hallo anggun selamat datang!";
+var input3 = "hallo ** selamat datang!";
+var input4 = "hallo Mariage889120! selamat datang!";
 
-   if (input1.valueOf.match(validasiHuruf)) {
-      hasil = "Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan" + input1;
-
-   } else {
-      hasil = "Sistem kami menolak untuk inputan berisi angka 23";
-      input1.focus();
-      return false;
-   }
+function testNama(string) {
+   return /\d/.test(string);
 }
 
-console.log(validation1());
+if (testNama(input1)) {
+   console.log("Sistem kami menolak untuk inputan berisi angka " + input1.match(/\d/g).join(""));
+
+} else {
+   console.log("Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan " + input1);
+}
+
+if (testNama(input2)) {
+   console.log("Sistem kami menolak untuk inputan berisi angka " + input2.match(/\d/g).join(""));
+
+} else {
+   console.log("Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan " + input2);
+}
+
+if (testNama(input3)) {
+   console.log("Sistem kami menolak untuk inputan berisi angka " + input3.match(/\d/g).join(""));
+
+} else {
+   console.log("Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan " + input3);
+}
+
+if (testNama(input4)) {
+   console.log("Sistem kami menolak untuk inputan berisi angka " + input4.match(/\d/g).join(""));
+
+} else {
+   console.log("Sistem memeriksa data anda valid dengan ketentuan kami dengan inputan " + input4);
+}

76a27e4e21f1c37016d00380fbf2b3972a95cefc
ilyasaaz <ilyasajir@gmail.com>
2022-07-12 13:54:07 +0700

    Ilyasa Azharil Muhajir : Test Kawah Edukasi Batch III

--- 3.js
+++ 3.js
@@ -3,14 +3,14 @@
 // Alamat Email: ilyasaazm@gmail.com
 // Soal Nomor 3
 
-const bintang = "*";
-const spasi = " ";
+const bintang= "*";
+const spasi= " ";
 console.log("Segitiga 1")
-for (var baris = 6, bbintang=0; baris > 0; baris--, bbintang++) {
+for (var baris= 6, bbintang=0; baris > 0; baris--, bbintang++) {
     console.log(bintang.repeat(bbintang+1)+spasi.repeat(baris));
 }
 
 console.log("\nSegitiga 2")
-for (var baris = 5, bbintang=1; baris > 0; baris--, bbintang++) {
+for (var baris= 5, bbintang=1; baris > 0; baris--, bbintang++) {
     console.log(spasi.repeat(baris-1)+bintang.repeat(bbintang));
 }

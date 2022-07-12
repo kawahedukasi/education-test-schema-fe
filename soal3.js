e848bde8963655ec5942ebf9f086cdb5dff0e476
dimasdy17 <dimas.dy31@gmail.com>
2022-07-12 14:00:47 +0700

    Dimas Dwi Yulianto : Test Kawah Edukasi bath III

---
+++ soal3.js
@@ -0,0 +1,23 @@
+function segitigaatas(panjang) {
+    let hasil = '';
+    for (let i = 0; i < panjang; i++) {
+        for (let j = 0; j <= i; j++) {
+            hasil += '* ';
+        }
+        hasil += '\n';
+    }
+    return hasil;
+}
+console.log(segitigaatas(6));
+
+function segitigabawah(panjang) {
+    let hasil = '';
+    for (let i = 0; i < panjang; i++) {
+        for (let j = panjang; j > i; j--) {
+            hasil += '* ';
+        }
+        hasil += '\n';
+    }
+    return hasil;
+}
+console.log(segitigabawah(6));
